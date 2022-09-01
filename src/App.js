d537d5a04856df8f1ceaaa9404d6210af67063bb
Nicko Sinnikov <nickosinnikov@outlook.com>
2022-09-01 14:10:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import './css/main.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+  }
+function ShopItemFunc(){
+  return(
+    <div class="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div class="description">
+        {item.descriptionFull}
+      </div>
+      <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+      <div class="divider"></div>
+      <div class="purchase-info">
+        <div class="price">{item.currency}{item.price}</div>
+        <button>Добавить в корзину</button>
+      </div>
     </div>
-  );
+  )
+}
+
+
+function App() {
+  
+  return (
+      <div className="container">
+        <div className="background-element">
+        </div>
+        <div className="highlight-window">
+          <div className='highlight-overlay'></div>
+        </div>
+        <div className="window">
+          <ShopItemFunc/>
+        </div>
+      </div>
+    )
 }
 
 export default App;
