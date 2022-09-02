536a39cd0fd1dfcc0abe1c77dc7ab4dd7cb86c68
Nicko Sinnikov <nickosinnikov@outlook.com>
2022-09-02 15:38:31 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
+import React from 'react';
 import './css/main.css';
 import './App.css';
+import ShopItemFunc from './component/ShopItemFunc';
+
 
 const item = {
-  brand: 'Tiger of Sweden',
-  title: 'Leonard coat',
-  description: 'Minimalistic coat in cotton-blend',
-  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-  price: 399,
-  currency: '£'
-  }
-function ShopItemFunc(){
-  return(
-    <div class="main-content">
-      <h2>{item.brand}</h2>
-      <h1>{item.title}</h1>
-      <h3>{item.description}</h3>
-      <div class="description">
-        {item.descriptionFull}
-      </div>
-      <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-      <div class="divider"></div>
-      <div class="purchase-info">
-        <div class="price">{item.currency}{item.price}</div>
-        <button>Добавить в корзину</button>
-      </div>
-    </div>
-  )
-}
-
-
+	brand: 'Tiger of Sweden',
+	title: 'Leonard coat',
+	description: 'Minimalistic coat in cotton-blend',
+	descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+	price: 399,
+	currency: '£'
+	}
 function App() {
   
   return (
@@ -39,7 +22,7 @@ function App() {
           <div className='highlight-overlay'></div>
         </div>
         <div className="window">
-          <ShopItemFunc/>
+          <ShopItemFunc item={item} />
         </div>
       </div>
     )
